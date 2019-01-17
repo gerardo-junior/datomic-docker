f07d6bad3aa2d674c11d0bf0562da7cb8b58d389
Gerardo Junior <me@gerardo-junior.com>
2019-01-16 23:17:51 -0300

    Adding host ip to datomic config

--- datamic-free/tools/entrypoint.sh
+++ datamic-free/tools/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed "/host=0.0.0.0/a alt-host=${ALT_HOST:-127.0.0.1}" -i transactor.properties
+sed "/host=0.0.0.0/a alt-host=$(ip route | awk 'NR==1 {print $3}')" -i transactor.properties
 
 if [ ! -z "$1" ]; then
     exec "$@"
