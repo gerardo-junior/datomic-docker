84fb62d2b6949b8cef5bd46a9f71ddb3b98d571a
Gerardo Junior <me@gerardo-junior.com>
2019-01-17 00:21:18 -0300

    Changing case label dockerfile

--- datomic-free/tools/entrypoint.sh
+++ datomic-free/tools/entrypoint.sh
@@ -3,7 +3,7 @@
 sed "/host=0.0.0.0/a alt-host=$(ip route | awk 'NR==1 {print $3}')" -i transactor.properties
 
 if [ ! -z "$1" ]; then
-    exec "$@"
-else 
     bin/transactor transactor.properties
+else 
+    exec "$@"
 fi
