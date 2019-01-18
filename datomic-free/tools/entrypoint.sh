2ef4c521ced31f88eba91159d493c34eeac37b8f
Gerardo Junior <me@gerardo-junior.com>
2019-01-17 22:32:53 -0300

    Adding empty test of parms

--- datomic-free/tools/entrypoint.sh
+++ datomic-free/tools/entrypoint.sh
@@ -3,9 +3,7 @@
 sed "/host=0.0.0.0/a alt-host=$(ip route | awk 'NR==1 {print $3}')" -i transactor.properties
 
 if [ ! -z "$1" ]; then
-    if [[ -z "$(which -- $1)" ]]; then
-        bin/transactor transactor.properties
-    else
-        exec "$@"
-    fi
+    exec bin/transactor transactor.properties
+else 
+    exec "$@"
 fi
